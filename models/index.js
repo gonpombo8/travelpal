cdbc3a89846eee4506b308bdb73bd54628b2aa14
Gonzalo Pombo <gonpombo@gmail.com>
2016-05-04 13:42:09 -0300

    console.log out

--- models/index.js
+++ models/index.js
@@ -7,7 +7,6 @@ var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
-console.log(process)
 if (process.env.HEROKU_POSTGRESQL_CRIMSON_URL) {
 	sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_CRIMSON_URL, {
 		logging: false,
